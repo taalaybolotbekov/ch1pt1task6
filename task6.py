83fc01c077c6bd1381477fa98bba83c35506ceb9
taalaybolotbekov <bolotbekovtaalay@gmail.com>
2020-03-10 12:37:36 +0600

    sixth commit

---
+++ task6.py
@@ -0,0 +1,21 @@
+def bigger_guy(one, two):
+    if one > two:
+        return one
+    elif one < two :
+        return two
+    elif one == two:
+        return one
+
+# DO NOT remove lines below here, this is designed to test your code.
+
+def test_bigger_guy():
+    assert bigger_guy(1, 2) == 2 
+    assert bigger_guy(10, 20) == 20 
+    assert bigger_guy(20, 10) == 20
+    assert bigger_guy(10, 10) == 10 
+    assert bigger_guy(2, 1) == 2 
+    assert bigger_guy('a', 'b') == 'b' 
+'b' is greater than 'a' 
+    print("YOUR CODE IS CORRECT!")
+# test your code by un-commenting the line(s) below
+test_bigger_guy()
